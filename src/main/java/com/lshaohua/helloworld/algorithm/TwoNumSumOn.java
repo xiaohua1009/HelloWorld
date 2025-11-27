af81cfc8afb0901f4cb5d91fa47d820aaa48b875
lsh <lshaohua@gmail.com>
2025-11-28 00:25:34 +0800

    twoNumSum

---
+++ src/main/java/com/lshaohua/helloworld/algorithm/TwoNumSumOn.java
@@ -0,0 +1,29 @@
+package com.lshaohua.helloworld.algorithm;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoNumSumOn {
+
+    public static void main(String[] args) {
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+        int[] result = twoSum(nums, target);
+        System.out.println(result[0] + " " + result[1]);
+    }
+
+    public static int[] twoSum(int[] nums, int target) {
+        int[] result = new int[2];
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                result[0] = map.get(complement);
+                result[1] = i;
+                return result;
+            }
+            map.put(nums[i], i);
+        }
+        return result;
+    }
+}
