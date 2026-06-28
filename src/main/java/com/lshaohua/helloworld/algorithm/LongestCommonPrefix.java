3da61179690a96f721ea1039fac50f2cfa8d8745
lsh <lshaohua@gmail.com>
2026-06-28 23:36:58 +0800

    longestCommonPrefix

---
+++ src/main/java/com/lshaohua/helloworld/algorithm/LongestCommonPrefix.java
@@ -0,0 +1,24 @@
+package com.lshaohua.helloworld.algorithm;
+
+public class LongestCommonPrefix {
+    public static void main(String[] args) {
+        System.out.println(longestCommonPrefix(new String[]{"flow", "flower", "flight"}));
+        System.out.printf("aaflower".indexOf("flow")+"");
+    }
+
+    public static String longestCommonPrefix(String[] strs) {
+        if (strs.length == 0) {
+            return "";
+        }
+        String prefix = strs[0];
+        for (int i = 1; i < strs.length; i++) {
+            while (strs[i].indexOf(prefix) != 0) {
+                prefix = prefix.substring(0, prefix.length() - 1);
+                if (prefix.isEmpty()) {
+                    return "";
+                }
+            }
+        }
+        return prefix;
+    }
+}
