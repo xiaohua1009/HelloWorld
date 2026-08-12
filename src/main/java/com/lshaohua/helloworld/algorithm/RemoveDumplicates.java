99259a468a12eeeb34ecd2078e419c094f95052e
lsh <lshaohua@gmail.com>
2026-08-13 00:24:18 +0800

    删除有序数组中的重复项

---
+++ src/main/java/com/lshaohua/helloworld/algorithm/RemoveDumplicates.java
@@ -0,0 +1,26 @@
+package com.lshaohua.helloworld.algorithm;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class RemoveDumplicates {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static int removeDuplicates(int[] nums) {
+        if (nums== null || nums.length == 0) {
+            return 0;
+        }
+        int slow = 0;
+        for (int fast = 1; fast < nums.length; fast++) {
+            if (nums[fast] != nums[slow]) {
+                slow++;
+                nums[slow] = nums[fast];
+            }
+        }
+
+        return slow + 1;
+    }
+}
