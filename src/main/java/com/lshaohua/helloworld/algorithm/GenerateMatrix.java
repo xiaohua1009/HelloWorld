9deea60f9f610f1b4dd79fc20f9dac7dc118fbc0
lsh <lshaohua@gmail.com>
2026-08-16 15:23:26 +0800

    旋转链表

---
+++ src/main/java/com/lshaohua/helloworld/algorithm/GenerateMatrix.java
@@ -0,0 +1,46 @@
+package com.lshaohua.helloworld.algorithm;
+
+import java.util.Arrays;
+
+public class GenerateMatrix {
+
+    public static void main(String[] args) {
+        int[][] result = generateMatrix(3);
+        for (int i = 0; i < result.length; i++) {
+            System.out.println(Arrays.toString(result[i]));
+        }
+    }
+
+    public static int[][] generateMatrix(int n) {
+        int[][] result = new int[n][n];
+        int num = 1;
+        int left = 0, right = n - 1, top = 0, bottom = n - 1;
+        while (left <= right && top <= bottom) {
+            for (int i = left; i <= right; i++) {
+                result[top][i] = num;
+                num++;
+            }
+            top++;
+            for (int i = top; i <= bottom; i++) {
+                result[i][right] = num;
+                num++;
+            }
+            right--;
+            if (top <= bottom) {
+                for (int i = right; i >= left; i--) {
+                    result[bottom][i] = num;
+                    num++;
+                }
+                bottom--;
+            }
+            if (left <= right) {
+                for (int i = bottom; i >= top; i--) {
+                    result[i][left] = num;
+                    num++;
+                }
+                left++;
+            }
+        }
+        return result;
+    }
+}
