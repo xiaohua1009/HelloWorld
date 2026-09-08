d80292f7c69187ae36cad2030755daa852a0c454
huashao <huashao@huashaodeMacBook-Pro.local>
2026-09-09 01:28:09 +0800

    InorderTraversal

---
+++ src/main/java/com/lshaohua/helloworld/algorithm/TreeNode.java
@@ -0,0 +1,26 @@
+package com.lshaohua.helloworld.algorithm;
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+
+
+
+
+
---
+++ src/main/java/com/lshaohua/helloworld/algorithm/_94InorderTraversal.java
@@ -0,0 +1,42 @@
+package com.lshaohua.helloworld.algorithm;
+
+import java.util.*;
+
+public class _94InorderTraversal {
+
+    public static List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        inorder(root, list);
+        return list;
+    }
+
+    public static void inorder(TreeNode root, List<Integer> list) {
+        if (root == null) {
+            return;
+        }
+        inorder(root.left, list);
+        list.add(root.val);
+        inorder(root.right, list);
+    }
+
+    public static List<Integer> inorderTraversal2(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        if (root == null) {
+            return list;
+        }
+        Deque<TreeNode> stack = new ArrayDeque<>();
+        TreeNode cur = root;
+        while (cur != null || !stack.isEmpty()) {
+            while (cur != null) {
+                stack.push(cur);
+                cur = cur.left;
+            }
+            cur = stack.pop();
+            list.add(cur.val);
+            cur = cur.right;
+        }
+        return list;
+    }
+
+
+}
